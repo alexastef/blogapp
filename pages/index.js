c20b342c2d6298dddcdfabb649ddadb3a3f35baa
Alexa Stefankiewicz <alexa.c.stef@gmail.com>
2021-02-16 13:36:49 -0800

    create blog posts and read from db

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,37 @@
-import Head from 'next/head'
+import { useState, useEffect } from "react";
+import fire from "../config/fire-config";
+import Head from "next/head";
+import CreatePost from "../components/CreatePost";
 
 const Home = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    fire.firestore()
+      .collection("blog")
+      .onSnapshot(snap => {
+        const blogs = snap.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data()
+        }));
+        setBlogs(blogs);
+      });
+  }, []);
+
+  console.log(blogs);
+
   return (
     <div>
       <Head>
         <title>Blog App</title>
       </Head>
       <h1>Alexa's Blog</h1>
+      <ul>
+        {blogs.map(blog =>
+          <li key={blog.id}>{blog.title}</li>
+        )}
+      </ul>
+      <CreatePost />
     </div>
   )
 }
